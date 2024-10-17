f55b8c372d0d50d79739f821aa931bf8a055bd64
Ole Bastian Løchen <42940656+olebaba@users.noreply.github.com>
2024-10-17 15:35:13 +0200

    tester minimalt oppsett (#17)

--- src/main/kotlin/no/nav/flex/auditlogger/App.kt
+++ src/main/kotlin/no/nav/flex/auditlogger/App.kt
@@ -2,10 +2,8 @@ package no.nav.flex.auditlogger
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.kafka.annotation.EnableKafka
 
 @SpringBootApplication
-@EnableKafka
 class Application
 
 fun main(args: Array<String>) {
--- src/main/kotlin/no/nav/flex/auditlogger/kafka/EventType.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.flex.auditlogger.kafka
-
-enum class EventType {
-    CREATE,
-    READ,
-    UPDATE,
-    DELETE,
-}
