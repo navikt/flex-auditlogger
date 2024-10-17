89dcd82de810a6e7fc1ae4acd7dd5eccd9e6a395
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-10-17 18:06:22 +0200

    bruker logback til config for auditlog

--- src/main/kotlin/no/nav/flex/auditlogger/Log.kt
+++ src/main/kotlin/no/nav/flex/auditlogger/Log.kt
@@ -2,7 +2,17 @@ package no.nav.flex.auditlogger
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 inline fun <reified T> T.logger(): Logger {
     return LoggerFactory.getLogger(T::class.java)
+}
+
+@Configuration
+class AuditLoggerConfig {
+    @Bean
+    fun auditLogger(): Logger {
+        return LoggerFactory.getLogger("auditLogger")
+    }
 }
---
+++ src/main/kotlin/no/nav/flex/auditlogger/kafka/AuditEntry.kt
@@ -0,0 +1,44 @@
+package no.nav.flex.auditlogger.kafka
+
+import java.net.URI
+import java.time.Instant
+
+/**
+ * @param utførtAv Nav-ident eller fnr på arbeidsgiver
+ * @param oppslagPå Fnr på person det gjøres oppslag på, eller organisasjon
+ * @param beskrivelse Beskrivelse av hva som er gjort, bør være "menneskelig lesbar"
+ */
+data class AuditEntry(
+    val fagsystem: String,
+    val appNavn: String,
+    val utførtAv: String,
+    val oppslagPå: String,
+    val eventType: EventType,
+    val forespørselTillatt: Boolean,
+    val oppslagUtførtTid: Instant,
+    val beskrivelse: String,
+    val requestUrl: URI,
+    val requestMethod: String,
+    val correlationId: String,
+) {
+    fun tilCEFFormat(): String {
+        return """
+            CEF:0|${this.fagsystem}|${this.appNavn}|1.0|${eventType.logString}|Sporingslogg|INFO|
+            flexString1=${if (this.forespørselTillatt) "Permit" else "Deny"} 
+            msg=${this.beskrivelse} 
+            request=${this.requestUrl} 
+            suid=${this.utførtAv} 
+            duid=${this.oppslagPå} 
+            requestMethod=${this.requestMethod} 
+            flexString1Label=Decision 
+            end=${this.oppslagUtførtTid}
+            """.trimIndent()
+    }
+}
+
+enum class EventType(val logString: String) {
+    CREATE("audit:create"),
+    READ("audit:access"),
+    UPDATE("audit:update"),
+    DELETE("audit:delete"),
+}
